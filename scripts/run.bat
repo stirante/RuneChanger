12e7ebd3b3c03bd7cebafc5e33fbd146cd59c9b6
stirante <brzozowski.s.piotr@gmail.com>
2019-10-31 20:09:29 +0100

    Change main jar name

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,2 +1,2 @@
 @echo off
-"image/bin/java" -cp "${project.build.finalName}.jar;lib/*" --add-exports=javafx.base/com.sun.javafx.reflect=ALL-UNNAMED --add-exports=javafx.graphics/com.sun.javafx.scene.layout=ALL-UNNAMED com.stirante.RuneChanger.RuneChanger
+"image/bin/java" -cp "${project.artifactId}.jar;lib/*" --add-exports=javafx.base/com.sun.javafx.reflect=ALL-UNNAMED --add-exports=javafx.graphics/com.sun.javafx.scene.layout=ALL-UNNAMED com.stirante.RuneChanger.RuneChanger
