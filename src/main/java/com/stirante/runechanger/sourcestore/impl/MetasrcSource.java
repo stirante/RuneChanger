98ffbaec664c3cb7d4d66f7fedbcc95243880994
PixelHir <pixelhir@outlook.com>
2020-09-20 12:33:04 +0200

    Added support for special gamemodes in MetasrcSource.

--- src/main/java/com/stirante/runechanger/sourcestore/impl/MetasrcSource.java
+++ src/main/java/com/stirante/runechanger/sourcestore/impl/MetasrcSource.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -57,14 +58,37 @@ public class MetasrcSource implements RuneSource {
 
 
         }
+
+        String specialGamemodeURL = CHAMPION_URL
+                .replace("%CHAMPION%", data.getChampion().getInternalName())
+                .replace("%LANE%", "");
+        // GETTING RUNES FOR SPECIAL GAMEMODES
+        String gamemodeKey = getSpecialGamemodeKey(data);
+
+        if(gamemodeKey != null) {
+            final String requestURL = specialGamemodeURL.replace("%MODE%", gamemodeKey);
+            try {
+                Document webPage = Jsoup.parse(new URL(requestURL), 10000);
+                RunePage r = extractRunes(webPage);
+                r.setChampion(data.getChampion());
+                r.setName(data.getGameMode().getName());
+                r.setSource(requestURL);
+                r.setSourceName("Metasrc");
+                pages.add(r);
+            } catch (Exception e) {
+                log.warn("Failed to get Metasrc runedata for special gamemode: " + data.getGameMode().getName());
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
     private RunePage extractRunes(Document webPage) {
         RunePage r = new RunePage();
-        Elements runes = webPage.select("div._yq1p7n._sjgjkw > div._sfh2p9 > div");
+        Elements runes = webPage.select("div._lop72r:nth-of-type(2) > div._sjgjkw:nth-of-type(2) > div._sfh2p9 > div");
         Elements mainRunes = runes.get(0).select("div._hmag7l > div._xdda66 > div._q8ue62");
         Elements secondaryRunes = runes.get(1).select("div._hmag7l > div._xdda66 > div._q8ue62");
-
         r.setMainStyle(Style.getByName(mainRunes.get(0).text()));
         r.setSubStyle(Style.getByName(secondaryRunes.get(0).text()));
 
@@ -118,6 +142,23 @@ public class MetasrcSource implements RuneSource {
                 return Modifier.RUNE_5007;
             case "healthscaling":
                 return Modifier.RUNE_5001;
+            default:
+                return null;
+        }
+    }
+
+    private String getSpecialGamemodeKey(GameData data) {
+        switch(data.getGameMode()) {
+            case ONEFORALL:
+                return "ofa";
+            case URF:
+                return "urf";
+            case ARAM:
+                return "aram";
+            case KINGPORO:
+                return "poro";
+            case NEXUSBLITZ:
+                return "blitz";
             default:
                 return null;
         }
