9c805c6bab93d3aa20d53e99c1b7affd1e65898c
Piotr Brzozowski <brzozowski.s.piotr@gmail.com>
2020-09-25 21:37:34 +0200

    Add methods for interacting with event tokens

--- src/main/java/com/stirante/runechanger/client/Loot.java
+++ src/main/java/com/stirante/runechanger/client/Loot.java
@@ -2,20 +2,28 @@ package com.stirante.runechanger.client;
 
 import com.stirante.eventbus.EventBus;
 import com.stirante.eventbus.Subscribe;
+import com.stirante.lolclient.ApiResponse;
 import com.stirante.lolclient.ClientApi;
 import com.stirante.runechanger.model.client.Champion;
 import generated.LolChampionsCollectionsChampionMinimal;
 import generated.LolCollectionsCollectionsChampionMastery;
 import generated.LolLootPlayerLoot;
+import generated.LolLootRecipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Loot extends ClientModule {
     private static final Logger log = LoggerFactory.getLogger(Loot.class);
+
+    /**
+     * List of ignored materials, when searching for tokens. Currently it ignores keys, key fragments and honor orbs.
+     */
+    private static final List<String> IGNORED_MATERIALS =
+            Arrays.asList("MATERIAL_329", "MATERIAL_key", "MATERIAL_key_fragment");
 
     public Loot(ClientApi api) {
         super(api);
@@ -35,7 +43,8 @@ public class Loot extends ClientModule {
     public int craftKeys() {
         try {
             LolLootPlayerLoot keyFragments = getApi()
-                    .executeGet("/lol-loot/v1/player-loot/MATERIAL_key_fragment", LolLootPlayerLoot.class).getResponseObject();
+                    .executeGet("/lol-loot/v1/player-loot/MATERIAL_key_fragment", LolLootPlayerLoot.class)
+                    .getResponseObject();
             if (keyFragments.count >= 3) {
                 getApi().executePost("/lol-loot/v1/recipes/MATERIAL_key_fragment_forge/craft?repeat=" +
                         keyFragments.count / 3, new String[]{"MATERIAL_key_fragment"});
@@ -72,7 +81,8 @@ public class Loot extends ClientModule {
     public void smartDisenchantChampions() {
         try {
             LolChampionsCollectionsChampionMinimal[] champions =
-                    getApi().executeGet("/lol-champions/v1/owned-champions-minimal", LolChampionsCollectionsChampionMinimal[].class).getResponseObject();
+                    getApi().executeGet("/lol-champions/v1/owned-champions-minimal", LolChampionsCollectionsChampionMinimal[].class)
+                            .getResponseObject();
             LolCollectionsCollectionsChampionMastery[] masteries =
                     getApi().executeGet("/lol-collections/v1/inventories/" + getCurrentSummoner().summonerId +
                             "/champion-mastery", LolCollectionsCollectionsChampionMastery[].class).getResponseObject();
@@ -122,4 +132,72 @@ public class Loot extends ClientModule {
             log.error("Exception occurred while smart disenchanting champions", e);
         }
     }
+
+    /**
+     * Returns a map of loot Ids and their count
+     *
+     * @return map of loot Id -> token count
+     */
+    public Map<String, Integer> getEventTokens() {
+        try {
+            ApiResponse<LolLootPlayerLoot[]> loot =
+                    getApi().executeGet("/lol-loot/v1/player-loot/", LolLootPlayerLoot[].class);
+            return Arrays.stream(loot.getResponseObject())
+                    .filter(lolLootPlayerLoot ->
+                            lolLootPlayerLoot.type.equalsIgnoreCase("MATERIAL") &&
+                                    !IGNORED_MATERIALS.contains(lolLootPlayerLoot.lootId))
+                    .collect(Collectors.toMap(
+                            lolLootPlayerLoot -> lolLootPlayerLoot.lootId,
+                            lolLootPlayerLoot -> lolLootPlayerLoot.count)
+                    );
+        } catch (IOException e) {
+            log.error("Exception occurred while getting map of event tokens", e);
+            return new HashMap<>();
+        }
+    }
+
+    /**
+     * Returns a map of token recipe names and their costs
+     *
+     * @param lootId loot id
+     * @return map of token recipe name -> recipe cost
+     */
+    public Map<String, Integer> getRecipes(String lootId) {
+        try {
+            ApiResponse<LolLootRecipe[]> loot =
+                    getApi().executeGet("/lol-loot/v1/recipes/initial-item/" + lootId, LolLootRecipe[].class);
+            return Arrays.stream(loot.getResponseObject())
+                    .filter(lolLootRecipe -> lolLootRecipe.slots.stream()
+                            .anyMatch(lolLootRecipeSlot -> lolLootRecipeSlot.lootIds.contains(lootId)))
+                    .collect(Collectors.toMap(
+                            lolLootPlayerLoot -> lolLootPlayerLoot.recipeName,
+                            lolLootPlayerLoot -> lolLootPlayerLoot.slots.stream()
+                                    .filter(lolLootRecipeSlot -> lolLootRecipeSlot.lootIds.contains(lootId))
+                                    .findFirst()
+                                    .orElseThrow().quantity)
+                    );
+        } catch (IOException e) {
+            log.error("Exception occurred while getting map of recipes", e);
+            return new HashMap<>();
+        }
+    }
+
+    /**
+     * Crafts a recipe
+     * @param recipeName recipe name
+     * @param lootId material Id to be consumed
+     * @param repeat how many times it should be crafted
+     * @return whether the crafting was successful
+     */
+    public boolean craftRecipe(String recipeName, String lootId, int repeat) {
+        try {
+            ApiResponse<Void> response = getApi().executePost(
+                    "/lol-loot/v1/recipes/" + recipeName + "/craft?repeat=" + repeat, new String[]{lootId});
+            return response.isOk();
+        } catch (IOException e) {
+            log.error("Exception occurred while crafting a recipe", e);
+            return false;
+        }
+    }
+
 }
