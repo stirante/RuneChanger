7cce80403072b332861f7b9abba9c0c971056d81
Piotr Brzozowski <brzozowski.s.piotr@gmail.com>
2021-02-07 00:22:10 +0100

    Decrease the usage of LoLTheory bandwidth

--- src/main/java/com/stirante/runechanger/sourcestore/impl/LolTheorySource.java
+++ src/main/java/com/stirante/runechanger/sourcestore/impl/LolTheorySource.java
@@ -14,14 +14,23 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class LolTheorySource implements RuneSource {
-    private static final Logger log = LoggerFactory.getLogger(LolTheorySource.class);
-    final String CHAMPION_URL = "https://loltheory-1b4da.firebaseio.com/%s/champions/%s.json";
+    final String ROLE_URL = "https://loltheory-1b4da.firebaseio.com/%s/champions/%s/info/role.json";
+    final String SELECTION_URL =
+            "https://loltheory-1b4da.firebaseio.com/%s/champions/%s/%s%sinfo/selection.json";
+    final String RUNES_URL =
+            "https://loltheory-1b4da.firebaseio.com/%s/champions/%s/%s%ssecondary_documents/runes_and_stat_shards.json";
     final String PATCH_URL = "https://loltheory-1b4da.firebaseio.com/%s.json?shallow=true";
     final String PUBLIC_URL = "https://loltheory.gg/champion/%s";
     private String patchString;
+
+    private Map<Champion, List<RunePage>> cache = new HashMap<>();
+
+    private int counter = 0;
 
     @Override
     public String getSourceName() {
@@ -33,28 +42,67 @@ public class LolTheorySource implements RuneSource {
         return "loltheory.gg";
     }
 
+    private List<Integer> getRoles(Champion champion) {
+        try {
+            String url = String.format(ROLE_URL, patchString, champion.getInternalName());
+//            System.out.println(url);
+            String json = Pipe.from(new URL(url).openStream())
+                    .toString();
+//            counter += json.length();
+            JsonArray roles = JsonParser.parseString(
+                    json).getAsJsonArray();
+            return StreamSupport.stream(roles.spliterator(), false)
+                    .map(JsonElement::getAsInt)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    private JsonObject getRunes(Champion champion, int role) {
+        try {
+            String items = "/";
+            for (int i = 0; i < 3; i++) {
+                String url = String.format(SELECTION_URL, patchString, champion.getInternalName(), role, items);
+//                System.out.println(url);
+                String json = Pipe.from(new URL(url).openStream())
+                        .toString();
+//                counter += json.length();
+                JsonArray list = JsonParser.parseString(
+                        json).getAsJsonArray();
+                items += list.get(0).getAsInt() + "/";
+            }
+            String url = String.format(RUNES_URL, patchString, champion.getInternalName(), role, items);
+//            System.out.println(url);
+            String json = Pipe.from(new URL(url).openStream())
+                    .toString();
+//            counter += json.length();
+            return JsonParser.parseString(
+                    json).getAsJsonObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @Override
     public void getRunesForGame(GameData data, SyncingListWrapper<RunePage> pages) {
+        if (cache.containsKey(data.getChampion())) {
+            pages.addAll(cache.get(data.getChampion()));
+        }
+        else {
+            cache.put(data.getChampion(), new ArrayList<>());
+        }
+//        counter = 0;
         if (patchString == null) {
             initPatchString();
         }
         try {
-            String json =
-                    Pipe.from(new URL(String.format(CHAMPION_URL, patchString, data.getChampion()
-                            .getInternalName())).openStream())
-                            .toString();
-            JsonObject obj = JsonParser.parseString(json).getAsJsonObject();
-            for (String s : obj.keySet()) {
-                Position pos = getPosition(s);
-                if (pos != null) {
-                    JsonObject item = obj.getAsJsonObject(s);
-                    while (!item.has("secondary_documents")) {
-                        item = item.getAsJsonObject(String.valueOf(item.getAsJsonObject("info")
-                                .getAsJsonArray("selection")
-                                .get(0)
-                                .getAsInt()));
-                    }
-                    item = item.getAsJsonObject("secondary_documents").getAsJsonObject("runes_and_stat_shards");
+            List<Integer> roles = getRoles(data.getChampion());
+            for (Integer role : roles) {
+                JsonObject item = getRunes(data.getChampion(), role);
+                if (item != null) {
                     RunePage p = new RunePage();
                     JsonArray mainRunes = item.getAsJsonObject("runes")
                             .getAsJsonArray("selection")
@@ -83,7 +131,7 @@ public class LolTheorySource implements RuneSource {
                     for (JsonElement r : shards) {
                         p.getModifiers().add(Modifier.getById(r.getAsInt()));
                     }
-                    p.setName(getPositionName(pos));
+                    p.setName(getPositionName(Objects.requireNonNull(getPosition(role))));
                     p.setChampion(data.getChampion());
                     p.setSource(String.format(PUBLIC_URL, data.getChampion().getInternalName().toLowerCase()));
                     p.setSourceName(getSourceName());
@@ -93,28 +141,29 @@ public class LolTheorySource implements RuneSource {
                         continue;
                     }
                     pages.add(p);
+                    cache.get(data.getChampion()).add(p);
                 }
             }
         } catch (Exception e) {
-            System.out.println(data.getChampion());
             e.printStackTrace();
         }
+//        System.out.println(counter);
     }
 
-    private static Position getPosition(String id) {
+    private static Position getPosition(int id) {
         switch (id) {
-            case "0":
+            case 0:
                 return Position.TOP;
-            case "1":
+            case 1:
                 return Position.JUNGLE;
-            case "2":
+            case 2:
                 return Position.MIDDLE;
-            case "3":
+            case 3:
                 return Position.BOTTOM;
-            case "4":
+            case 4:
                 return Position.UTILITY;
             default:
-                return null;
+                return Position.UNSELECTED;
         }
     }
 
